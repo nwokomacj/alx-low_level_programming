2cf6132563354a118f113436ca1021e7fe2c45f3
nwokomacj <nwokomachidiebube123@gmail.com>
2022-10-05 22:56:10 -0700

    a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * argstortr - concatenates all arguments of the program
+ * argstostr - concatenates all arguments of the program
  * @ac: argument count
  * @av: pointer to array of size ac
  * Return: NULL if ac == 0 or av == null, pointer to new string
