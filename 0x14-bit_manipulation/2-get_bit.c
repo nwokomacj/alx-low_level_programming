1d253e12bc4e3d970e984d4c355398e7e784b4ce
nwokomacj <nwokomachidiebube123@gmail.com>
2023-12-15 13:27:16 -0800

    new commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit, bin;
+	int bit;
 
 	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
-	bin = n >> index;
-	bit = bin & 1;
+	bit = (n >> index) & 1;
 	return (bit);
 }
